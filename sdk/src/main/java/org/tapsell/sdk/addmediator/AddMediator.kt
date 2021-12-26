a7bf8409fee3261378a5e6cd83367047eb95fde3
Morteza Nedaei <nedaei1375@gmail.com>
2021-12-27 00:26:36 +0330

    add base class for AddMediator

---
+++ sdk/src/main/java/org/tapsell/sdk/addmediator/AddMediator.kt
@@ -0,0 +1,19 @@
+package org.tapsell.sdk.addmediator
+
+class AddMediator {
+
+    companion object {
+
+        @JvmStatic
+        fun initialize() {
+        }
+
+        @JvmStatic
+        fun requestAdd() {
+        }
+
+        @JvmStatic
+        fun showAdd() {
+        }
+    }
+}

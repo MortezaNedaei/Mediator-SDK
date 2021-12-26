852f8d24affd2f2adbe00a416b15221a7a00d043
Morteza Nedaei <nedaei1375@gmail.com>
2021-12-27 00:25:58 +0330

    add required constants

---
+++ sdk/src/main/java/org/tapsell/sdk/utils/Constants.kt
@@ -0,0 +1,10 @@
+package org.tapsell.sdk.utils
+
+internal object Constants {
+
+    const val PreferencesName = "tapsell.org"
+    const val DBName = "tapsell.db"
+
+    const val ENDPOINT_AD_NETWORKS = "v1/adnetworks"
+    const val ENDPOINT_REWARDED_WATERFALL = "v1/rewarded"
+}

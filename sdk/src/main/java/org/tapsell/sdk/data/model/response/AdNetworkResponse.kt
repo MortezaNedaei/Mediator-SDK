e6fd8df652a7e22d6ec68252f166b683d71490ab
Morteza Nedaei <nedaei1375@gmail.com>
2021-12-27 00:25:44 +0330

    add required remote models

---
+++ sdk/src/main/java/org/tapsell/sdk/data/model/response/AdNetworkResponse.kt
@@ -0,0 +1,14 @@
+package org.tapsell.sdk.data.model.response
+
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+internal data class AdNetworkResponse(
+    @Json(name = "id")
+    val id: String,
+    @Json(name = "name")
+    val name: String
+) : Parcelable
---
+++ sdk/src/main/java/org/tapsell/sdk/data/model/response/RewardedWaterfallResponse.kt
@@ -0,0 +1,14 @@
+package org.tapsell.sdk.data.model.response
+
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+internal data class RewardedWaterfallResponse(
+    @Json(name = "type")
+    val type: String,
+    @Json(name = "waterfall")
+    val waterfall: List<WaterfallResponse>
+) : Parcelable

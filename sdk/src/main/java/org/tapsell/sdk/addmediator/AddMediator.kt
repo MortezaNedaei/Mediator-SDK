f9d15d494f5af38b3c43dc18e289fe70452f875f
Morteza Nedaei <nedaei1375@gmail.com>
2021-12-28 01:46:56 +0330

    add some logic to get adNetworks from api and initialize each one

--- sdk/src/main/java/org/tapsell/sdk/addmediator/AddMediator.kt
+++ sdk/src/main/java/org/tapsell/sdk/addmediator/AddMediator.kt
@@ -1,19 +1,62 @@
 package org.tapsell.sdk.addmediator
 
-class AddMediator {
+import android.content.Context
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tapsell.sdk.data.model.enums.AdType
+import org.tapsell.sdk.data.model.response.AdNetworkItem
+import org.tapsell.sdk.data.repository.AdsRepositoryImpl
 
-    companion object {
 
-        @JvmStatic
-        fun initialize() {
+class AddMediator constructor(
+    private val context: Context
+) : IAddMediator {
+
+
+//    companion object {
+//
+//        @JvmStatic
+//        fun initialize() {
+//        }
+//
+//        @JvmStatic
+//        fun requestAdd() {
+//        }
+//
+//        @JvmStatic
+//        fun showAdd() {
+//        }
+//    }
+
+    override fun initialize(context: Context) {
+        CoroutineScope(Dispatchers.IO).launch {
+            getAndInitializeAdNetworks()
         }
 
-        @JvmStatic
-        fun requestAdd() {
-        }
+    }
 
-        @JvmStatic
-        fun showAdd() {
+    override fun requestAd() {
+    }
+
+    override fun showAdd() {
+    }
+
+    private suspend fun getAndInitializeAdNetworks() {
+        val adNetworks = AdsRepositoryImpl.getAdNetworks()
+        adNetworks.forEach { adNetworkResponse: AdNetworkItem ->
+            when (adNetworkResponse.name) {
+                AdType.UNITY.title -> {
+                    UnityAdsManager.initialize(context, adNetworkResponse.id)
+                }
+                AdType.TAPSELL.title -> {
+                    TapsellAdsManager.initialize(context, adNetworkResponse.id)
+                }
+                else -> {
+                    throw IllegalStateException("this ad type does not specified")
+                }
+            }
         }
     }
 }

5b043672fd15cd82bdb8ba8a207bb3d7c05dfaba
Morteza Nedaei <nedaei1375@gmail.com>
2021-12-30 17:10:08 +0330

    add method to get active waterfall from db if exist + expiration handling (delete expires from db)

--- sdk/src/main/java/org/tapsell/sdk/data/datasource/WaterfallDataSource.kt
+++ sdk/src/main/java/org/tapsell/sdk/data/datasource/WaterfallDataSource.kt
@@ -1,19 +1,41 @@
 package org.tapsell.sdk.data.datasource
 
-import androidx.lifecycle.LiveData
+import android.util.Log
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import org.tapsell.sdk.data.local.db.user.WaterfallEntity
 import org.tapsell.sdk.data.model.response.WaterfallResponse
 import org.tapsell.sdk.data.model.response.toLocalEntity
 import org.tapsell.sdk.data.repository.LocalWaterfallRepositoryImpl
 import org.tapsell.sdk.data.repository.RemoteWaterfallRepositoryImpl
+import org.tapsell.sdk.utils.Utils.isWaterfallExpired
 
 
 internal object WaterfallDataSource {
 
+    val TAG = this::class.java.simpleName
+
     suspend fun fetchAndStore() {
         val remoteList: Flow<WaterfallResponse> = fetchAll()
         storeAll(remoteList)
+    }
+
+    suspend fun getActiveWaterfallOrNull(): WaterfallEntity? {
+        var activeWaterfall: WaterfallEntity? = null
+        readAllRecords().onEach { waterfall: WaterfallEntity ->
+            if (isWaterfallExpired(waterfall.timestamp)) {
+                // delete from db if expired
+                deleteRecord(waterfall)
+            } else {
+                activeWaterfall = waterfall
+                return@onEach
+            }
+        }.catch { e: Throwable ->
+            Log.e("Error: $TAG :", e.message?.toString() ?: "")
+        }.collect()
+        return activeWaterfall
     }
 
     private suspend fun fetchAll(): Flow<WaterfallResponse> {
@@ -24,7 +46,7 @@ internal object WaterfallDataSource {
         LocalWaterfallRepositoryImpl.insertAll(flowList.toLocalEntity())
     }
 
-    suspend fun readAllRecords(): LiveData<List<WaterfallEntity>> {
+    private suspend fun readAllRecords(): Flow<WaterfallEntity> {
         return LocalWaterfallRepositoryImpl.getAll()
     }
 

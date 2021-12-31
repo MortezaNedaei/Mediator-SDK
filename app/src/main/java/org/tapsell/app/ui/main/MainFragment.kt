0fa053f9e01026a4ce145ee88bec3b465017e7c6
Morteza Nedaei <nedaei1375@gmail.com>
2021-12-31 10:49:00 +0330

    add some ui

--- app/src/main/java/org/tapsell/app/ui/main/MainFragment.kt
+++ app/src/main/java/org/tapsell/app/ui/main/MainFragment.kt
@@ -1,21 +1,21 @@
 package org.tapsell.app.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
-import org.tapsell.app.R
-import org.tapsell.sdk.addmediator.AddMediator
-import org.tapsell.sdk.addmediator.IAddMediator
-import javax.inject.Inject
+import org.tapsell.app.databinding.FragmentMainBinding
+import org.tapsell.sdk.addmediator.AdMediator
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = MainFragment()
@@ -24,14 +24,41 @@ class MainFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels<MainViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnRequestAd.setOnClickListener {
+            requestAd()
+        }
+
+        binding.btnShowAd.setOnClickListener {
+            showAd()
+        }
+    }
+
+    private fun requestAd() {
+        AdMediator
+            .getInstance()
+            .requestAd(requireActivity())
+    }
+
+    private fun showAd() {
+        AdMediator
+            .getInstance()
+            .showAdd(requireActivity())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
